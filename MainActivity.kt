d66fc1cec57f9447d9bc300d098525b5d1a919a6
ttu2441 <116522719+ttu2441@users.noreply.github.com>
2023-04-11 15:37:21 -0400

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.helloandroid
 
 /*To import the android app combat activity*/
+//Importing the bundle, widget, button, and edittext
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,7 @@ import android.widget.EditText
 class MainActivity : AppCompatActivity() {
 
     //This shows Property must be initialized or be abstract
+    //The main Kotlin activity to show if the button and edittext shows null or not
     internal var ans: Button? = null
     internal var num1: EditText? = null
     internal var num2: EditText? = null
